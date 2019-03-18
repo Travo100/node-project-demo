f91919761db2ded4826dd630256feaa421439027
Travo100 <Travo100@gmail.com>
2019-03-18 14:09:42 -0700

    Added spotify

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello from node!");
+require('dotenv').config();
+
+console.log(process.env.SUPER_SECRET);
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_CLIENT_ID,
+  secret: process.env.SPOTIFY_SECRET_KEY
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
